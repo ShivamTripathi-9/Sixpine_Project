e8489d6e7996118e6c674556ccbf62e91b2e23ab
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-09-29 12:53:45 +0530

    correct css code

--- src/pages/CareerPage.tsx
+++ src/pages/CareerPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";  
 import Navigation from "../components/Navigation.jsx";
-import NavigationDown from "../components/Navigationdown.jsx"; 
+
 
 import Footer from "../components/footer.jsx";
 import Career from "../components/career.jsx";
@@ -14,7 +14,7 @@ const CareerPage: React.FC = () => {
     <div>
       <Navbar />
       <Navigation />
-      <NavigationDown />
+      {/* <NavigationDown /> */}
       <Career />
       
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";  
 import Navigation from "../components/Navigation.jsx";
-import NavigationDown from "../components/Navigationdown.jsx"; 
+
 
 import Footer from "../components/footer.jsx";
 import Login from "../components/login.jsx";
@@ -15,7 +15,7 @@ const AboutPage: React.FC = () => {
     <div>
       <Navbar />
       <Navigation />
-      <NavigationDown />
+      {/* <NavigationDown /> */}
       <Login />
       
 
